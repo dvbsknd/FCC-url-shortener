2e8172fb34791cac0939a266bc3dc90bb497894d
Dave Baskind <dave@picketstudio.com>
2020-08-17 08:13:30 +1000

    Retrieve the supplied URL from POST body

--- routers/api.js
+++ routers/api.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded());
 
 router.post('/shorturl/new', (req, res) => {
-  const original_url = 'www.google.com';
+  const original_url = req.body.url;
   const short_url = 1;
   res.json({ original_url, short_url });
 });
