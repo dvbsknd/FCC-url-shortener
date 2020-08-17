e280a46fb7c01f55d31cad18d3ef83f0da520802
Dave Baskind <dave@picketstudio.com>
2020-08-17 19:58:13 +1000

    Skipped file in previous commit

--- controllers/shortUrl.js
+++ controllers/shortUrl.js
@@ -5,22 +5,23 @@ const hashId = require('../utils/hashId.js');
 module.exports = function (req, res) {
   // Validate the supplied URL meets expected
   // pattern
-  const { url, error } = validateUrl(req.body.url);
-  if (error) res.json({ error });
-  else {
-    // Generate a unique ID
-    const code = hashId(url);
-    // Instantiate a new ShortUrl
-    const shortUrl = new ShortUrl({ url, code });
-    // Store the URL and ID in the database
-    // and send the response once done/failed
-    shortUrl.save((error, data) => {
-      if (error) res.json(error);
-      else {
-        const { url, code } = data;
-        res.json({ url, code });
-        console.log(`Created shortcode ${code} for ${url}`);
-      }
-    });
-  }
+  validateUrl(req.body.url, (error, url) => {
+    if (error) res.json({ error });
+    else {
+      // Generate a unique ID
+      const code = hashId(url);
+      // Instantiate a new ShortUrl
+      const shortUrl = new ShortUrl({ url, code });
+      // Store the URL and ID in the database
+      // and send the response once done/failed
+      shortUrl.save((error, data) => {
+        if (error) res.json({ database_error: error });
+        else {
+          const { url, code } = data;
+          res.json({ url, code });
+          console.log(`Created shortcode ${code} for ${url}`);
+        }
+      });
+    }
+  });
 }
